53051f55839adc7e895e1351fdb2f86d7d13f973
Ashley-L <52459805+Ashley-L@users.noreply.github.com>
2021-04-06 23:01:36 -0400

    Menu toggle.

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// Menu toggle
+
+let menuButton = document.querySelector(`.button-menu`);
+let menuIcon = document.querySelector(`.menu`);
+let nav = document.querySelector(`.nav`);
+var menuOpened = Boolean(false);
+
+// Toggle menu open/close
+menuButton.addEventListener('click', function(event) {
+    // console.log(`clicked`);
+    nav.classList.toggle(`menu__opened`);
+
+})
+
